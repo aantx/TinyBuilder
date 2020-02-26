7f89c8adb0696c46a139b5571ca64bf58212d771
aantx <aantx@live.ru>
2020-02-26 15:39:29 +0300

    Solved encoding problems

--- test.ps1
+++ test.ps1
@@ -1,8 +1,17 @@
 ﻿Remove-Variable * -Scope local -ErrorAction SilentlyContinue
 $w = [System.Diagnostics.Stopwatch]::StartNew()
-
-$a=(Get-Item $PSScriptRoot\MonitorHw.exe).VersionInfo.ProductVersionRaw#.ToString()
-#ps2exe -inputFile $PSScriptRoot\MonitorHw.ps1 -outputFile $PSScriptRoot\MonitorHw.exe -x64 -requireAdmin -iconFile $PSScriptRoot\5.ico -title MonitorHW -product MonitorHW -copyright SPK -trademark SPK -company SPK  -description "Мониторинг оборудования компьютера" -version 1.0
-
+$Path = (Split-Path -Parent $PSScriptRoot)+"\MonitorHW"
+Set-Location $Path
+$a=(Get-Item $Path\MonitorHw.exe).VersionInfo.ProductVersionRaw
+$b= [System.Version]::new($a.Major,$a.Minor+2,$a.Build,$a.Revision+1)
+#& "git log" "-1 HEAD"
+#ps2exe -inputFile $Path\MonitorHw.ps1 -outputFile $Path\MonitorHw.exe -x64 -requireAdmin -iconFile $Path\5.ico -title MonitorHW -product MonitorHW -copyright SPK -trademark SPK -company SPK  -description "Мониторинг оборудования компьютера" -version "$b"
+"was"+$a
+"become"+$b
+$gitHist = (git log --format="%ai`t%h`t%an`t%s" -n 1) | ConvertFrom-Csv -Delimiter "`t" -Header ("Date", "CommitId", "Author", "Subject")
 $w.stop()
+$w
+
+
+#git log -p -- .\MonitorHw.exe
 $w.ElapsedMillisecondss
