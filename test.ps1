31683a5e545dbe5c268ddac0ba566a435d6b3a73
aantx <aantx@live.ru>
2020-03-10 20:00:10 +0300

    worked on a code appearance

--- test.ps1
+++ test.ps1
@@ -1,24 +1,10 @@
 ﻿#Remove-Variable * -Scope local -ErrorAction SilentlyContinue
-# $w = [System.Diagnostics.Stopwatch]::StartNew()
-# $Path = (Split-Path -Parent $PSScriptRoot)+"\MonitorHW"
-# Set-Location $Path
-# $a=(Get-Item $Path\MonitorHw.exe).VersionInfo.ProductVersionRaw
-# $b= [System.Version]::new($a.Major,$a.Minor+2,$a.Build,$a.Revision+1)
-# #& "git log" "-1 HEAD"
-# #ps2exe -inputFile $Path\MonitorHw.ps1 -outputFile $Path\MonitorHw.exe -x64 -requireAdmin -iconFile $Path\5.ico -title MonitorHW -product MonitorHW -copyright SPK -trademark SPK -company SPK  -description "Мониторинг оборудования компьютера" -version "$b"
-# "was"+$a
-# "become"+$b
-# $gitHist = (git log --format="%ai`t%h`t%an`t%s" -n 1) | ConvertFrom-Csv -Delimiter "`t" -Header ("Date", "CommitId", "Author", "Subject")
-# $w.stop()
-# $w
+Set-Location D:\prg\Powershell\MonitorHW
 
-
-# git log -p --format="%ai" -n 1 -- .\MonitorHw.exe
-# $w.ElapsedMillisecondss
 $VersionOld=[System.Version]::new(1, 0, 0, 0)
 
 #Predict non-existing file
-$VersionOld = (Get-Item .\*.exe).VersionInfo.ProductVersionRaw
+$VersionOld = (Get-Item .\MonitorHw.exe).VersionInfo.ProductVersionRaw
 
 $Major = $VersionOld.Major
 $Minor = $VersionOld.Minor
@@ -28,11 +14,14 @@ $Revision = $VersionOld.Revision
 #checkout to master
 $a = git log --format="%ai" -n 1 -- .\MonitorHw.exe
 git log --format="%B" --since $a --reverse -- .\MonitorHw.ps1 |
-Where-Object { $_ -ne "" } | ForEach-Object {
-        if ($_ -match "-A") {$Major+=1; $Minor=0; $Build = 0;$Revision = 0}
-        elseif ($_ -match "-B") { $Minor+=1; $Build = 0; $Revision =0}
-        elseif ($_ -match "-C") { $Build+=1; $Revision = 0}
-        else {$Revision+=1}
-    }
+    Where-Object { $_ -ne "" } | ForEach-Object {
+            if ($_ -match "-A") {$Major+=1; $Minor=0; $Build = 0;$Revision = 0}
+                elseif ($_ -match "-B") { $Minor+=1; $Build = 0; $Revision =0}
+                elseif ($_ -match "-C") { $Build+=1; $Revision = 0}
+            else {$Revision+=1}
+        }
 $VersionNew = [System.Version]::new($Major, $Minor, $Build, $Revision -join ".")
-(Get-Content .\*.ps1) -replace $VersionOld.ToString(),$VersionNew.ToString()|Write-Host
+
+(Get-Content .\MonitorHw.ps1) -replace $VersionOld.ToString(), $VersionNew.ToString()|
+    out-null #Write-Host
+#ps2exe -inputFile $Path\MonitorHw.ps1 -outputFile $Path\MonitorHw.exe -x64 -requireAdmin -iconFile $Path\5.ico -title MonitorHW -product MonitorHW -copyright SPK -trademark SPK -company SPK  -description "Мониторинг оборудования компьютера" -version "$b"
