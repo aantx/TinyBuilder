ab272bf1c2deb79a0a1bb4b55dc8b4cfe4ba3181
aantx <aantx@live.ru>
2020-03-16 16:32:55 +0300

    added support of changing scriptbody

--- TB.ps1
+++ TB.ps1
@@ -1,4 +1,6 @@
 ﻿param($Path, $File)
+#$Path = "D:\prg\Powershell\MonitorHW"
+#$File="MonitorHw"
 Set-Location $Path -ErrorAction Stop
 $VersionOld=[System.Version]::new(1, 0, 0, 0)
 
@@ -14,31 +16,45 @@ $Revision = $VersionOld.Revision
 
 #checkout to master
 $PrevVersion="HEAD"
-$LastExeDate = git log --format="%ai" -n 1 -- .\$File.exe
+$LastExeDate = &git log --format=`"%ai`" -n 1 -- ".\$File.exe"
 git log --format="%B" --reverse --since=$LastExeDate -- .\$File.ps1 |
-    Where-Object { $_ -ne "" } | ForEach-Object {
-        if ($_ -match "-A") {$Major+=1; $Minor=0; $Build = 0;$Revision = 0}
-        elseif ($_ -match "-B") { $Minor+=1; $Build = 0; $Revision =0}
-        elseif ($_ -match "-C") { $Build+=1; $Revision = 0}
+    Where-Object { $item -ne "" } | ForEach-Object {
+        if ($item -match "-A") {$Major+=1; $Minor=0; $Build = 0;$Revision = 0}
+        elseif ($item -match "-B") { $Minor+=1; $Build = 0; $Revision =0}
+        elseif ($item -match "-C") { $Build+=1; $Revision = 0}
         else {$Revision+=1}
         $PrevVersion+="^"
     }
 $VersionNew = [System.Version]::new($Major, $Minor, $Build, $Revision -join ".")
 $VersionNew
-$ScriptBody = Get-Content .\$File.ps1 -Encoding "UTF8"
-$ScriptBody.Replace("([В-я]{6})\:([0-9]*\.){3}[0-9]*", ('$1:' + $VersionNew.ToString()))
-$rege = [regex] "(([Д-я]{8})\:([0-9]{2}\.){2}[0-9]{4})"
-$rega=[regex]"([А-я]*)\: ([\s\w\.\,\:А-я]+?\n\n)"
-$rege.Replace($str, ('$2:' + (Get-Date -Format "dd.MM.yyyy").ToString()), 1)
-$str
-$rega.Replace($str, ('FINISHED'), 1)
+$gitOut='$1: ' + (git log -1 --format="%B" -- .\$File.ps1)
+$ScriptBody=Get-Content .\$File.ps1 -Encoding "UTF8"
+foreach ($i in (0..100)){
+    $ScriptBody[$i] = ([regex] "(Версия)\:([0-9]*\.){3}[0-9]*").Replace($ScriptBody[$i], '$1:' + $VersionNew.ToString(), 1)
+    $ScriptBody[$i] = ([regex] "(Изменения)\: ([ \w\.\,\:A-zА-я]*)").Replace($ScriptBody[$i], $gitOut , 1)
+    $ScriptBody[$i] = ([regex] "(создания)\:([0-9]{2}\.){2}[0-9]{4}").Replace($ScriptBody[$i], '$1:' + (Get-Date -Format "dd.MM.yyyy").ToString(), 1)
+}
+#$ScriptBody> .\1.txt
+#Set-Content $ScriptBody -Path .\1.ps1 -Force -Encoding "UTF8"
+
 <#
+        $process=Start-Process $gitPath -ArgumentList $ArgumentsList -NoNewWindow -PassThru -Wait -RedirectStandardError $gitErrorPath -RedirectStandardOutput $gitOutputPath
+
+
+$ScriptBody.Replace("(Версия)\:([0-9]*\.){3}[0-9]*", ('$1:' + $VersionNew.ToString()))
+$rege = [regex] "((создания)\:([0-9]{2}\.){2}[0-9]{4})"
+$rega = [regex]"(Изменения)\: ([ \w\.\,\:A-zА-я]*)"
+$rege.Replace($str, ('$2:' + (Get-Date -Format "dd.MM.yyyy").ToString()), 1)
+$VersString = '$1:' + $VersionNew.ToString()
+$DateString='$1:' + (Get-Date -Format "dd.MM.yyyy").ToString()
+$MessageString = '$1: ' + (git log -1 --format="%B" -- .\$File.ps1)
 (Get-Content .\$File.ps1 -Encoding "UTF8") `
     -replace "([В-я]{6})\:([0-9]*\.){3}[0-9]*", ('$1:' + $VersionNew.ToString()) `
     -replace "","" `
     -replace "", ""|
-    Set-Content .\$File.ps1 -Force -Encoding "UTF8"
+
 #>
+Set-Content .\$File.ps1 -Value $ScriptBody -Force -Encoding "UTF8"
 ps2exe -inputFile $Path\$File.ps1 -outputFile $Path\$File.exe -x64 -requireAdmin `
     -iconFile $Path\icon.ico -title $File -product $File `
     -copyright $OldFileAttr.LegalCopyright -trademark $OldFileAttr.LegalTrademarks `
