77a07ed3afd2479330ff42aa92a7379db5d6dcd7
aantx <aantx@live.ru>
2020-03-18 20:16:24 +0300

    brushed script from garbage

--- TB.ps1
+++ TB.ps1
@@ -1,9 +1,8 @@
 ﻿param($Path, $File)
-#$Path = "D:\prg\Powershell\MonitorHW"
-#$File="MonitorHw"
 Set-Location $Path -ErrorAction Stop
 $VersionOld=[System.Version]::new(1, 0, 0, 0)
 
+#If path do not contains basic exe
 if (-not (Test-Path -Path .\$File.exe)){
     Write-Host "Исполняемый файл не найден. Введите"
     $Major = Read-Host "Введите мажорную версию:"
@@ -15,10 +14,8 @@ if (-not (Test-Path -Path .\$File.exe)){
         -title $File -product $File -version "$VersionNew" | Out-Null
     exit
 }
-#Predict non-existing file
 
 $OldFileAttr=(Get-Item .\$File.exe).VersionInfo
-
 $VersionOld = $OldFileAttr.ProductVersionRaw
 $Major = $VersionOld.Major
 $Minor = $VersionOld.Minor
@@ -39,7 +36,6 @@ git log --format="%B" --reverse --since=$LastExeDate -- .\$File.ps1 |
 $VersionNew = [System.Version]::new($Major, $Minor, $Build, $Revision -join ".")
 $VersionNew
 [Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("UTF-8")
-
 $GitOut = '$1: ' + (git log -1 --format="%B" -- .\$File.ps1)
 
 $ScriptBody=Get-Content .\$File.ps1 -Encoding "UTF8"
@@ -49,26 +45,6 @@ foreach ($i in (0..100)){
     $ScriptBody[$i] = ([regex] "(Изменения)\: ([ \w\.\,\:A-zА-я]*)").Replace($ScriptBody[$i], $GitOut , 1)
     $ScriptBody[$i] = ([regex] "(создания)\:([0-9]{2}\.){2}[0-9]{4}").Replace($ScriptBody[$i], '$1:' + (Get-Date -Format "dd.MM.yyyy").ToString(), 1)
 }
-#$ScriptBody> .\1.txt
-#Set-Content $ScriptBody -Path .\1.ps1 -Force -Encoding "UTF8"
-
-<#
-        $process=Start-Process $gitPath -ArgumentList $ArgumentsList -NoNewWindow -PassThru -Wait -RedirectStandardError $gitErrorPath -RedirectStandardOutput $gitOutputPath
-
-
-$ScriptBody.Replace("(Версия)\:([0-9]*\.){3}[0-9]*", ('$1:' + $VersionNew.ToString()))
-$rege = [regex] "((создания)\:([0-9]{2}\.){2}[0-9]{4})"
-$rega = [regex]"(Изменения)\: ([ \w\.\,\:A-zА-я]*)"
-$rege.Replace($str, ('$2:' + (Get-Date -Format "dd.MM.yyyy").ToString()), 1)
-$VersString = '$1:' + $VersionNew.ToString()
-$DateString='$1:' + (Get-Date -Format "dd.MM.yyyy").ToString()
-$MessageString = '$1: ' + (git log -1 --format="%B" -- .\$File.ps1)
-(Get-Content .\$File.ps1 -Encoding "UTF8") `
-    -replace "([В-я]{6})\:([0-9]*\.){3}[0-9]*", ('$1:' + $VersionNew.ToString()) `
-    -replace "","" `
-    -replace "", ""|
-
-#>
 Set-Content .\$File.ps1 -Value $ScriptBody -Force -Encoding "UTF8"
 ps2exe -inputFile $Path\$File.ps1 -outputFile $Path\$File.exe -x64 -requireAdmin `
     -iconFile $Path\icon.ico -title $File -product $File `
